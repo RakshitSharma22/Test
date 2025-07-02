196aa3e9cc8726a26e87868b38f118775958e23a
RakshitSharma22 <167458440+RakshitSharma22@users.noreply.github.com>
2025-07-02 23:46:55 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,23 @@
 hello
 abe yeh toh upload aur copy hi ni hori image
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+1 ->  (2) 
+2 -> This registers the imported component that serves as the app's entry point.
+
+3->useNativeDriver: true
+
+4->
+5->Any type
+6->
+
+7->initialState  
+
+8->RCTRootView  
+9->1, 2, and 5  
+10->4. A pattern for structurally connecting two incompatible interfaces by wrapping one interface with another  
+11->All of these
+13->By stringifying the JSON object  
+  
   
